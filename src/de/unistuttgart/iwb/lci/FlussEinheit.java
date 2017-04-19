98d5248b163f158b7edc316518854c9876195493
a <a@141.58.24.213>
2017-04-19 10:08:50 +0200

    zwei Enumations

---
+++ src/de/unistuttgart/iwb/lci/FlussEinheit.java
@@ -0,0 +1,17 @@
+/*	
+ * Beispielprojekt LCI (Sachbilanz)
+ * Wintersemester 2016/2017
+ */
+
+package de.unistuttgart.iwb.lci;
+
+/**
+ * @author Dr.-Ing. Joachim Schwarte
+ * @version 0.9
+ */
+
+public enum FlussEinheit {
+	kg,
+	g,
+	Stk
+}
