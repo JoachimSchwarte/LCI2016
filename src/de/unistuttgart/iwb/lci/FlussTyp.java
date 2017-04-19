44747ccf5acd6105e0f56a4431debf2f5185cfd0
a <a@141.58.24.213>
2017-04-19 10:12:26 +0200

    zwei Enumations (2)

---
+++ src/de/unistuttgart/iwb/lci/FlussTyp.java
@@ -0,0 +1,16 @@
+/*	
+ * Beispielprojekt LCI (Sachbilanz)
+ * Wintersemester 2016/2017
+ */
+
+package de.unistuttgart.iwb.lci;
+
+/**
+ * @author Dr.-Ing. Joachim Schwarte
+ * @version 0.9
+ */
+
+public enum FlussTyp {
+	Elementar,
+	Produkt
+}
